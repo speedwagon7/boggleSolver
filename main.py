8dfd69c804d2c3ad229de68468dc1850d7541e25
hayden.poon <hayden.poon@fdmgroup.com>
2024-01-31 01:27:49 +0000

    2nd commit

--- main.py
+++ main.py
@@ -1,153 +1,108 @@
+from typing import List, Set
 
-class WordFinder: 
 
-    class Coordinate: 
-                def __init__(self, x, y): 
-                    self.x = x 
-                    self.y = y 
+class WordFinder:
+    class Coordinate:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
 
-    class TrieNode: 
-
-        def __init__(self, letter, position): 
-
+    class TrieNode:
+        def __init__(self, letter, position):
             self.word = position
-            self.word += letter 
-            self.children = {} 
-            self.used_positions = {} 
-            self.is_word = False    
+            self.word += letter
+            self.children = {}
+            self.used_positions = {}
+            self.is_word = False
 
-        def insert(self, position, parentNode): 
+        def insert(self, letter, position, parentNode) -> str:
+            # insert node if not used already
 
-            #insert node if not used already 
+            if position not in parentNode.used_positions:
+                # add to used positions if using it
+                appended_used_positions = parentNode.used_positions.copy()
+                appended_used_positions[position] = True
 
-            if position not in parentNode.used_positions: 
+                # Create new node and add to children of parent node
+                parentNode.children[position] = WordFinder.TrieNode(letter, position)
 
-                #add to used positions if using it 
-                appended_used_positions = parentNode.used_positions.copy() 
-                appended_used_positions[position] = True 
+            return parentNode.children[position].word
 
-                #add to children of parent node 
-                parentNode.children[position] = TrieNode(grid[position], position) 
+    class Trie:
+        def __init__(self, letter, position):
+            self.root = WordFinder.TrieNode(letter, position)
 
-                
+    def __init__(self, wordlist: List[str]):
+        self.wordlist = wordlist
 
+    def find_words(self, board: str) -> Set[str]:
+        print("Hello world!")
+        board = "EATE\nLXRR\nARTR\nITSE\n"
+        answers = set(())
 
+        # Convert string into grid dictionary
+        grid = {}
+        row = 0
 
-
-
-    class Trie(): 
-
-        def __init__(self, position): 
-
-            self.root = TrieNode(position) 
-
-    def __init__(self, wordlist: List[str]): 
-
-        self.wordlist = wordlist 
-
-    def find_words(self, board: str) -> Set[str]: 
-
-        print("Hello world!") 
-
-        board = "EATE\nLXRR\nARTR\nITSE\n" 
-
-        answers = set(()) 
-
-        # Convert string into grid dictionary 
-        grid = {} 
-
-        row = 0 
-
-        for i in range(len(board)): 
-
-            if board[i] not in ("/", "n"): 
-
-                row += 1 
-
-                i += 1 
-
-                continue 
-
-            coordinate = Coordinate(i, row) 
-
+        for i in range(len(board)):
+            if board[i] == ("/"):
+                row += 1
+                i += 1
+                continue
+            coordinate = WordFinder.Coordinate(i, row)
             grid[coordinate] = board[i].lower()
 
-        # Loop through grid 
+        # Loop through grid
         max_length = len(board)
-
         counter = 0
+
+        for key in grid:
+            trie = WordFinder.Trie(grid[key], key)
+            insert_adjacent_squares(trie)
+
+        def insert_adjacent_squares(trieNode):
+            while counter < max_length:
+                directions = [
+                    (-1, -1),
+                    (0, -1),
+                    (1, -1),  # top left, top middle, top right
+                    (1, 0),  # middle right
+                    (1, 1),
+                    (0, 1),
+                    (-1, 1),  # bottom right, bottom middle, bottom left
+                    (-1, 0),  # middle left
+                ]
+
+                for direction in directions:
+                    new_x, new_y = (
+                        coordinate.x + direction[0],
+                        coordinate.y + direction[1],
+                    )
+                    new_coordinate = WordFinder.Coordinate(new_x, new_y)
+
+                    # check if coordinate is in grid and coordinate contains a letter
+                    if new_coordinate in grid and grid[new_coordinate] != " ":
+                        letter = grid[new_coordinate]
+                        #creates new node
+                        new_node_word = trieNode.insert(
+                            letter, new_coordinate, trieNode
+                        )
         
-        for key in grid: 
-
-            trie = Trie(key) 
-
-            insert_adjacent_squares(trie) 
-
-            def insert_adjacent_squares(trieNode): 
-
-                while counter<max_length: 
-
-                    #top left 
-                    top_left = (coordinate.x - 1, coordinate.y -1) 
-                    if top_left in grid:
-                        newNode = TrieNode()
-                        trieNode.insert(grid[top_left]) 
-
-                    #if word is found in trie then add to final solution 
-                    if self.word in wordlist: 
-
-                        answers.add(self.word) 
-                    #top middle 
-                    top_middle = (coordinate.x, coordinate.y -1) 
-                    if top_middle in grid:
-
-                        trieNode.insert(grid[top_middle]) 
-
-                    #top right 
-                    top_right = (coordinate.x + 1, coordinate.y -1) 
-                    if top_right in grid:
-
-                        trieNode.insert(grid[top_right]) 
-
-                    #middle right 
-                    middle_right = (coordinate.x + 1, coordinate.y ) 
-                    if middle_right in grid:
-
-                        trieNode.insert(grid[middle_right]) 
-
-                    #bottom right 
-                    bottom_right = (coordinate.x + 1, coordinate.y + 1) 
-                    if bottom_right in grid:
-
-                        trieNode.insert(grid[bottom_right]) 
-
-                    #bottom middle 
-                    bottom_middle = (coordinate.x, coordinate.y + 1) 
-                    if bottom_middle in grid:
-                        trieNode.insert(grid[bottom_middle]) 
-
-                    #bottom left 
-                    bottom_left = (coordinate.x - 1, coordinate.y + 1) 
-                    if bottom_left in grid:
-                        trieNode.insert(grid[bottom_left]) 
-
-
-                    #middle left 
-                    middle_left = (coordinate.x - 1, coordinate.y -1) 
-                    if middle_left in grid:
-                        trieNode.insert(grid[middle_left]) 
-         
-        return answers 
- 
+        counter += 1
+        
+        def search_using_wordlist():
+            #to implement
+            print("search")
 def main():
     wordlist = ["eat", "art"]
     wordFinder = WordFinder(wordlist)
-    
+
     board = "EATE\nLXRR\nARTR\nITSE\n"
 
-    wordFinder.find_words(board)
+    for word in wordFinder.find_words(board):
+        print(word)
+
 
 if __name__ == "__main__":
     main()
 from ast import List, Set
- 
